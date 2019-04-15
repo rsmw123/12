d6cd8534470c201645db0d859d31ce7497fc148c
rsmw123 <49241358+rsmw123@users.noreply.github.com>
2019-04-15 14:24:09 +0300

    Add files via upload

--- link.js
+++ link.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
  client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
+    if (message.content.startsWith(" ���� ")) {
         message.channel.createInvite({
         thing: true,
         maxUses: 5,
@@ -21,10 +21,10 @@ const client = new Discord.Client();
               const Embed11 = new Discord.RichEmbed()
         .setColor("RANDOM")
         
-    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 5 **")
       message.author.sendEmbed(Embed11)
     }
 }); 
 
 
-client.login("توكن البوت")
+client.login(""NTEwNDYzMDI0MTkzMzM5Mzkz.XLI-5w.1BG1l3BZVXV-2BfOhm3GfWBl_7c"")
